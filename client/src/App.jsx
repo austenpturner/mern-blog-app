6ec2a5d47cc30a76d43b889318cb2fb45cc35bfe
Austen Turner <austenpturner@msn.com>
2024-07-16 09:33:20 -0700

    add header elements and nav routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/header";
+import Home from "./pages/home";
+import AddNewBlog from "./pages/add-blog";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/add-blog" element={<AddNewBlog />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
