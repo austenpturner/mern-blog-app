08414be7807648222875a74096559fe38cbfd259
Austen Turner <austenpturner@msn.com>
2024-07-16 09:39:27 -0700

    update add blog page, add styles

--- client/src/pages/add-blog/index.jsx
+++ client/src/pages/add-blog/index.jsx
@@ -1,7 +1,24 @@
+import classes from "./styles.module.css";
+
 export default function AddNewBlog() {
   return (
-    <div>
+    <div className={classes.wrapper}>
       <h2>Add New Blog</h2>
+      <div className={classes.formWrapper}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Enter blog title..."
+          id="title"
+        />
+        <textarea
+          name="description"
+          placeholder="Enter blog description..."
+          id="description"
+          type="text"
+        />
+        <button>Add New Blog</button>
+      </div>
     </div>
   );
 }
